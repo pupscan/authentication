0e77e2c070eb81ccd1c56bafbc325dc0e5ef91cc
Thibaut Mottet <thibaut.mottet@7next.fr>
2017-08-04 19:32:10 +0200

    Accept preflight request

--- src/main/kotlin/com/puspcan/authentication/AuthenticationApplication.kt
+++ src/main/kotlin/com/puspcan/authentication/AuthenticationApplication.kt
@@ -1,8 +1,10 @@
-package com.example.authentication
+package com.puspcan.authentication
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.security.core.userdetails.User
@@ -13,10 +15,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.security.Principal
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 
 @EnableResourceServer
@@ -27,6 +33,27 @@ fun main(args: Array<String>) {
     SpringApplication.run(AuthenticationApplication::class.java, *args)
 }
 
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class Cors : Filter {
+    override fun doFilter(req: ServletRequest, resp: ServletResponse, chain: FilterChain) {
+        val response = resp as HttpServletResponse
+        val request = req as HttpServletRequest
+        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+        response.setHeader("Access-Control-Max-Age", "3600")
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN")
+
+        if ("OPTIONS".equals(request.method, ignoreCase = true)) {
+            response.status = HttpServletResponse.SC_OK
+        } else {
+            chain.doFilter(req, resp)
+        }
+    }
+
+    override fun init(p0: FilterConfig?) {}
+    override fun destroy() {}
+}
 
 @RestController
 class PrincipalRestController {
--- src/test/kotlin/com/puspcan/authentication/AuthenticationApplicationTests.kt
+++ src/test/kotlin/com/puspcan/authentication/AuthenticationApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.authentication
+package com.puspcan.authentication
 
 import org.junit.Test
 import org.junit.runner.RunWith
